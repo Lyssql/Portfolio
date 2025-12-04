0e1f84e57bfad68d4d1e486e4749d376691262e2
Lyssql <ulysse.garnier.lfgeb@gmail.com>
2025-12-05 00:56:38 +0100

    Prier me permettra peut-être de déployer le site

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    // Alias "@" vers la racine du projet
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
 };
 
 module.exports = nextConfig;
